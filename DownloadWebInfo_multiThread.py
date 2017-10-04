66d7ef5601efc8d3764939851e934537e4ae204e
YW <ywkent_nwu@163.com>
2017-10-04 22:02:47 +0900

    V2.0
    V2.0

--- DownloadWebInfo_multiThread.py
+++ DownloadWebInfo_multiThread.py
@@ -7,12 +7,14 @@ global LOG
 global list_StockCodes
 global local_stockCode
 global mutex
+global THREAD_NUM
 
 LOG = 1
 list_StockCodes = []
 FILEPATH_BASE2 = "D:\\python_SRC\\Stock_SRC\\tmpData\\20171001\\"
 local_stockCode = threading.local()
 mutex = threading.Lock()
+THREAD_NUM = 4
 
 def DownloadWebInfoStart(sFilePath = FILEPATH_BASE2):
     global mutex
@@ -26,7 +28,7 @@ def DownloadWebInfoStart(sFilePath = FILEPATH_BASE2):
         local_stockCode.stock = stockCode
         local_stockCode.timeStart = timeStart1
         print("ThreadName:[%s], stock:[%s]" % (threading.current_thread().name, local_stockCode.stock ))
-        if stockCode > "600979" and stockCode < "603309":
+        if stockCode > "600979" and stockCode < "603268":
             time.sleep(3)
             downLoadWebInfo(sFilePath)
 
@@ -42,45 +44,25 @@ def downLoadWebInfo(sFilePath):
     WriteFile(PathTmp, htmlinfo)
     print("time: %d " % (time.time() - local_stockCode.timeStart))
 
-
-'''
-    global LOG
-    global FILEPATH_LOG
-    print ("BaseFilePath: " + sFilePath )
-    for stockCode in list_StockCodes:
-        print ("code:[%s]" % stockCode)
-        stockCode = str(stockCode).zfill(6)
-        if stockCode > "600703" and stockCode < "603355":
-            web = "http://vip.stock.finance.sina.com.cn/corp/go.php/vCI_StockHolder/stockid/%s.phtml" \
-                           % (stockCode)
-            print("web:[%s]" % web)
-            timeStart1 = time.time()
-            htmlinfo = getHtml(web)
-            PathTmp = sFilePath + stockCode + ".txt"
-            print("FilePath: " + PathTmp)
-            WriteFile(PathTmp, htmlinfo)
-            print("time: %d " % (time.time() - timeStart1))
-'''
-
-def run():
-    global LOG
+def run(listStockCodes):
+    global list_StockCodes
     print("GetWebInfo Start")
-    lStockCode = []
-    lStockCode = lGetStockCodes()
-    nStockCodeListLen = len(lStockCode)
-    if LOG == 1:
-        print("nStockCodeListLen:", nStockCodeListLen)
-        print(lStockCode)
-    DownloadWebInfo(lStockCode)
+    list_StockCodes = listStockCodes
+    for k in range(THREAD_NUM):
+        new_thread = threading.Thread(target = DownloadWebInfoStart)
+        new_thread.start()
+        record_thread.append(new_thread)
+    for thread in record_thread:
+        thread.join()
     print("GetWebInfo End")
 
 if __name__ == '__main__':
     print("GetWebInfo Start")
     list_StockCodes = lGetStockCodes()
-    t1 = threading.Thread(target = DownloadWebInfoStart)
-    t2 = threading.Thread(target = DownloadWebInfoStart)
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
+    for k in range(THREAD_NUM):
+        new_thread = threading.Thread(target = DownloadWebInfoStart)
+        new_thread.start()
+        record_thread.append(new_thread)
+    for thread in record_thread:
+        thread.join()
     print("GetWebInfo End")
